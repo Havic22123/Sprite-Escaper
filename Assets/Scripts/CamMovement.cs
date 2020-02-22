5d75a0f951d1e5163a572d444b8e083afd8edcf3
Jonathan <Jonablox@gmail.com>
2020-02-21 23:44:54 -0700

    Camera Follow

---
+++ Assets/Scripts/CamMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    private Transform tf;
+    public float movementSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        // Aim at the player at start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, tf.position, movementSpeed * Time.deltaTime);
+        // Always move forward
+    }
+}
